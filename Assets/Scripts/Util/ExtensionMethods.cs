08e1dcb1acc3c4c5aa4a9c9402859d5211980ab9
Mark Alberts <mark1996a@hotmail.com>
2015-05-27 14:33:45 +0200

    Added basic menu

---
+++ Assets/Scripts/Util/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ExtensionMethods {
+    private static System.Random random = new System.Random();
+
+    public static void Shuffle(this IList list) {
+        int n = list.Count;
+
+        while (n > 1) {
+            n--;
+
+            int k = random.Next(n + 1);
+            System.Object value = list[k];
+
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    public static bool EqualsIgnoreCase(this string text, string compareTo) {
+        return text.Equals(compareTo, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
---
+++ Assets/Scripts/Util/ScrollbarExtension.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScrollbarExtension : MonoBehaviour {
+    public Scrollbar scrollbar;
+    public Text scrollbarText;
+
+    private float stepSize;
+
+    void Awake() {
+        stepSize = 1F / scrollbar.numberOfSteps;
+    }
+
+    void Start() {
+        OnValueChanged(scrollbar.value);
+    }
+
+    public void IncreaseValue() {
+        scrollbar.value = Mathf.Clamp01(scrollbar.value + stepSize);
+    }
+
+    public void DecreaseValue() {
+        scrollbar.value = Mathf.Clamp01(scrollbar.value - stepSize);
+    }
+
+    public void OnValueChanged(float value) {
+        if (scrollbarText != null) {
+            scrollbarText.text = (scrollbar.value * 100) + "%";
+        }
+    }
+}
