6f73fcf2bca781eb11843baf2e74d65bbe920901
Mark Alberts <mark1996a@hotmail.com>
2015-06-22 10:01:16 +0200

    Fixed camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
     public float screenMovePercentage = 0.1f;
+    public float moveSpeed = 1f;
+    public bool enableDragRotate, enableDragMove, enableCursorMove;
 
     Transform mTrans;
     Vector3 mMouse;
@@ -17,19 +19,21 @@ public class CameraControl : MonoBehaviour {
         mTargetPos = mTrans.position;
         mTargetEuler = mTrans.rotation.eulerAngles;
 
-        xPixels = Screen.width * screenMovePercentage;
-        yPixels = Screen.height * screenMovePercentage;
+        leftPixels = Screen.width * screenMovePercentage;
+        rightPixels = Screen.width * (1.0f - screenMovePercentage);
+        upPixels = Screen.height * screenMovePercentage;
+        downPixels = Screen.height * (1.0f - screenMovePercentage);
     }
 
     void Update() {
         Vector3 delta = Input.mousePosition - mMouse;
         mMouse = Input.mousePosition;
 
-        if (Input.GetMouseButton(0)) {
+        if (enableDragRotate && Input.GetMouseButton(0)) {
             mTargetEuler.y += Time.deltaTime * 10f * delta.x;
         }
 
-        if (Input.GetMouseButton(1)) {
+        if (enableDragMove && Input.GetMouseButton(1)) {
             Vector3 dir = transform.rotation * Vector3.forward;
             dir.y = 0f;
             dir.Normalize();
@@ -37,9 +41,20 @@ public class CameraControl : MonoBehaviour {
             mTargetPos += rot * new Vector3(delta.x * 0.1f, 0f, delta.y * 0.1f);
         }
 
-        print(mMouse.ToString());
-        if (mMouse.x < xPixels || mMouse.x >  || mMouse.y < cursorMoveDistance || mMouse.y > 1 - cursorMoveDistance) {
-            print("in range");
+        if (enableCursorMove && (mMouse.x < leftPixels || mMouse.x > rightPixels || mMouse.y < upPixels || mMouse.y > downPixels)) {
+            Vector3 dir = transform.rotation * Vector3.forward;
+            dir.y = 0f;
+            dir.Normalize();
+            Quaternion rot = Quaternion.LookRotation(dir);
+            Vector3 distance = new Vector3(0,0,0);
+
+            if (mMouse.x < leftPixels || mMouse.x > rightPixels)
+                distance.x = mMouse.x < leftPixels ? -moveSpeed : moveSpeed;
+
+            if (mMouse.y < upPixels || mMouse.y > downPixels)
+                distance.z = mMouse.y < upPixels ? -moveSpeed : moveSpeed;
+
+            mTargetPos += rot * distance;
         }
 
         float deltaTime = Time.deltaTime * 8f;
