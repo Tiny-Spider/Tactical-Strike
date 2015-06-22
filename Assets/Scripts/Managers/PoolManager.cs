58de3a529c51caf08bd0603ad1a17f17b834a61e
Mark Alberts <mark1996a@hotmail.com>
2015-06-22 10:19:31 +0200

    Update

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public static class PoolManager : MonoBehaviour {
+public class PoolManager : MonoBehaviour {
 
     public Dictionary<string, Unit> unitPool = new Dictionary<string, Unit>();
     public Dictionary<string, Structure> structurePool = new Dictionary<string, Structure>();
